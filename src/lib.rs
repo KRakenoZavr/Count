c652a789a5a5e4da9af44092e8f455557fb8810f
Arman Kuzembay <kuzembay@gmail.com>
2023-05-28 14:25:09 +0600

    feat: normalized 1 query

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use reqwest::Client;
+use serde;
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Coin {
+    amount: String,
+    denom: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct QueryBalance {
+    balance: Coin,
+}
+
+pub async fn get_account_balance(address: String, denom: String) -> QueryBalance {
+    let url = format!("https://gnfd-testnet-fullnode-tendermint-us.bnbchain.org/cosmos/bank/v1beta1/balances/{address}/by_denom?denom={denom}");
+
+    let client = Client::new();
+
+    let response = client.get(url).send().await.unwrap();
+
+    let jsoned = response.json::<QueryBalance>().await.unwrap();
+
+    println!("{:#?}", jsoned);
+
+    jsoned
+}
