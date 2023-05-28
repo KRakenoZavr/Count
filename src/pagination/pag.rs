760093dfca2ed67d2801950fa4b32b1cb585cb6d
Arman Kuzembay <kuzembay@gmail.com>
2023-05-28 16:05:31 +0600

    f

--- src/pagination/pag.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Pagination {
-    next_key: String,
-    total: String,
-}
